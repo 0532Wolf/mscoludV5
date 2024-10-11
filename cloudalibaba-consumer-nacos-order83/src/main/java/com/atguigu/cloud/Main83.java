d1241507ab86a365945c52093a564976a1d3a80b
0532Wolf <gouxiangxun@Gmail.com>
2024-10-11 23:33:40 +0800

    sentinel使用

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/Main83.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/Main83.java
@@ -2,8 +2,12 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class Main83 {
 
     public static void main(String[] args) {
