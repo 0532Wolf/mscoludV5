81dffc76f08609bd72a2da5d2b4c7a325a2c500a
0532Wolf <gouxiangxun@Gmail.com>
2024-10-11 23:40:12 +0800

    sentinel使用

---
+++ cloudalibaba-sentinel-gateway9528/src/main/java/com/atguigu/cloud/main9528.java
@@ -0,0 +1,15 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class main9528 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(main9528.class, args);
+    }
+
+}
